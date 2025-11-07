b3c3100f9b010b852f29eb50270d496373df66fa
Voitenko Illia <79865476+IlyaVoitenko@users.noreply.github.com>
2025-11-07 13:21:18 +0100

    added db and create method

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,36 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class CategoriesService {
-  private readonly categories: CreateCategoryDto[] = [];
+  constructor(private readonly prisma: PrismaService) {}
+  create(data: CreateCategoryDto) {
+    if (!data.name) throw new Error('Category name cannot be empty');
 
-  create(category: CreateCategoryDto) {
-    this.categories.push(category);
-    return category;
-  }
-
-  findAll() {
-    return this.categories;
-  }
-
-  findOne(id: number) {
-    return this.categories.find((category) => category.id === id);
-  }
-
-  update(id: number, category: UpdateCategoryDto) {
-    const index = this.categories.findIndex((category) => category.id === id);
-    if (index === -1) throw new Error('Category not found');
-
-    this.categories[index] = { ...this.categories[index], ...category };
-    return this.categories[index];
-  }
-
-  remove(id: number) {
-    const index = this.categories.findIndex((category) => category.id === id);
-    if (index !== -1) {
-      return this.categories.splice(index, 1);
-    }
+    return this.prisma.category.create({ data });
   }
 }
